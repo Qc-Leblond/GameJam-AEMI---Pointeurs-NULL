816ea4a64b852037cd7be46f489cf4b99aeb33f8
Qc-Leblond <antoineqc@hotmail.com>
2014-10-10 22:13:19 -0400

    Slightly_Better_Controls

--- PointeursNULL_GameJam2/Assets/Script/Character_Controller.cs
+++ PointeursNULL_GameJam2/Assets/Script/Character_Controller.cs
@@ -11,6 +11,9 @@ public class Character_Controller : MonoBehaviour
     private bool onGround;
     private float groundCheckRadius = 0.1f;
 
+    private bool Button1Down;
+    private bool Button2Down;
+
     void Awake()
     {
         SpeedForce = 250f;
@@ -29,11 +32,35 @@ public class Character_Controller : MonoBehaviour
         {
             rigidbody2D.AddForce(new Vector2(SpeedForce * h, 0f));
         }
+
+        if (Input.GetAxis("PowerUp1") == 0) Button1Down = false;
+        else if (Input.GetAxis("PowerUp1") == 1 && !Button1Down)
+        {
+            Button1Down = true;
+            Debug.Log("Power Up 1");
+        }
+        else if (Input.GetAxis("PowerUp1") == -1 && !Button1Down)
+        {
+            Button1Down = true;
+            Debug.Log("Power Up 2");
+        }
+
+        if (Input.GetAxis("PowerUp2") == 0) Button2Down = false;
+        else if (Input.GetAxis("PowerUp2") == 1 && !Button2Down)
+        {
+            Button2Down = true;
+            Debug.Log("Power Up 3");
+        }
+        else if (Input.GetAxis("PowerUp2") == -1 && !Button2Down)
+        {
+            Button2Down = true;
+            Debug.Log("Power Up 4");
+        }
     }
 
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        if (Input.GetButtonDown("Jump") && onGround)
         {
             rigidbody2D.AddForce(new Vector2(0f, JumpForce));
         }
