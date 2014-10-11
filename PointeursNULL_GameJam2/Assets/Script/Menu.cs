12145e1db786e69152332507309bea0ab91a9e97
Tinaud <mathieu.naud@hotmail.com>
2014-10-10 21:57:24 -0400

    Menu (Jouer, Controles et Quitter)

--- PointeursNULL_GameJam2/Assets/Script/Menu.cs
+++ PointeursNULL_GameJam2/Assets/Script/Menu.cs
@@ -3,18 +3,33 @@ using System.Collections;
 
 public class Menu : MonoBehaviour 
 {
+	public bool Control = false, Quit = false;
 	void OnGUI () 
 	{
-		GUI.Box(new Rect(100,100,100,90), "Menu");
+		GUI.Box(new Rect(Screen.width/2 - 50,Screen.height/2 - 135,100,120), "Menu");
 
-		if(GUI.Button(new Rect(110,130,80,20), "Jouer")) 
+		if(GUI.Button(new Rect(Screen.width/2 - 40,Screen.height/2 - 105,80,20), "Jouer")) 
 		{
-			Application.LoadLevel("Scene1");
+			Application.LoadLevel("PlatformTest");
 		}
 			
-		if(GUI.Button(new Rect(110,160,80,20), "Bouton 2")) 
+		if(GUI.Button(new Rect(Screen.width/2 - 40,Screen.height/2 - 75,80,20), "Controles")) 
 		{
-			Debug.Log("Action 2");
+			Control = true;
 		}
+		if (GUI.Button (new Rect (Screen.width / 2 - 40, Screen.height / 2 - 45, 80, 20), "Quitter")) 
+		{
+			Quit = true;
+		}
+		if(Control)
+			ControlGUI ();
+		if (Quit)
+			Application.Quit ();
+			
+	}
+	void ControlGUI()
+	{
+		GUI.Box(new Rect(Screen.width/2 - 200,Screen.height/2,400,130), "Controles\n\n -Utilisez l'analog pour vous diriger de gauche a droite\n\n" +
+			"-Appuyez sur le boutton X pour sauter\n\n" + "-Utilisez la croix directionnelle pour utiliser un pouvoir");
 	}
 }
