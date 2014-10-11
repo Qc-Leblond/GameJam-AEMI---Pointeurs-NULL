2ed8f22367a08ca926f61f909a401b3488000b60
Tinaud <mathieu.naud@hotmail.com>
2014-10-10 20:20:03 -0400

    Début menu

---
+++ PointeursNULL_GameJam2/Assets/Script/Menu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour 
+{
+	void OnGUI () 
+	{
+		GUI.Box(new Rect(100,100,100,90), "Menu");
+
+		if(GUI.Button(new Rect(110,130,80,20), "Jouer")) 
+		{
+			Application.LoadLevel("Scene1");
+		}
+			
+		if(GUI.Button(new Rect(110,160,80,20), "Bouton 2")) 
+		{
+			Debug.Log("Action 2");
+		}
+	}
+}
