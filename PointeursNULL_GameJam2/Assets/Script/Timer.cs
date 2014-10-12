0eb3b7379efb67bc802e01358c6f9ce16fd2235e
Philippe Rheaume <trucphil85@hotmail.com>
2014-10-12 10:17:07 -0400

    Sprites

--- PointeursNULL_GameJam2/Assets/Script/Timer.cs
+++ PointeursNULL_GameJam2/Assets/Script/Timer.cs
@@ -28,8 +28,8 @@ public class Timer : MonoBehaviour
         {
             GUI.Box(new Rect((Screen.width / 2) - 30, 50, 60, 50), "Temps \n" + ((int)timeLeft));
         }
-		GUI.Box (new Rect (100, Screen.height - 100, 50, 50), "Score \n"+ZPoints.ToString());
-		GUI.Box (new Rect (Screen.width - 100, Screen.height-100, 50, 50), "Score \n"+HPoints);
+		GUI.Label (new Rect (100, Screen.height - 100, 50, 50), "Score \n"+ZPoints.ToString());
+		GUI.Label (new Rect (Screen.width - 100, Screen.height-100, 50, 50), "Score \n"+HPoints.ToString());
 	}
 
     public void NewTimer()
