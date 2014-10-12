d98a79a5ec379e4bf789a3fd7872b59a62b6ac5b
Qc-Leblond <antoineqc@hotmail.com>
2014-10-12 11:20:26 -0400

    CommitFinalMap

---
+++ PointeursNULL_GameJam2/Assets/Script/DeathZoning.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathZoning : MonoBehaviour 
+{
+    public GameObject Main;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Zombie")
+        {
+            Main.GetComponent<Game_Main>().ZombieDeath(other.gameObject);
+        }
+        if (other.tag == "Human")
+        {
+            Main.GetComponent<Game_Main>().HumanDeath(other.gameObject);
+        }
+    }
+}
